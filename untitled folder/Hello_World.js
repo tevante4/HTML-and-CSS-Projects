b4ad5e90cd886f56bcfb83b34ce1b802dbaaafeb
Tevante Swanston <tevswany@gmail.com>
2022-04-03 06:15:22 -0700

    Animations

---
+++ untitled folder/Hello_World.js
@@ -0,0 +1,8 @@
+let greeting = 'Hello World!';
+    console.log(greeting)
+
+let greeting2 = 'Welcome to Node.js';
+    console.log(greeting2)
+
+let sentence1 = 'Learning how to debug code with the debugger';
+    console.log(sentence1);
